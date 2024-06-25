0f6f4662e6d775a37a37ad433502e75b69dfaf5e
Abhay Balip <abhay.balip@gmail.com>
2024-06-25 19:36:57 +0530

    expredd

--- Index.js
+++ Index.js
@@ -1,4 +1,12 @@
 const print = (data)=> console.log(data)
 
-print('Hi')
+const express = require('express')
 
+const app = express(express.static(__dirname+'/Asset'))
+
+app.get('/',function(req,res){
+    res.sendFile(__dirname+'/Index.html')
+})
+
+
+app.listen(1000)
